4614a37b6d6ebed30474d8d1a762f6e1f8ed86e0
Riley <101227583+Slimey-Riley@users.noreply.github.com>
2023-09-30 21:00:11 +0000

    asdasf

--- client/src/infrastructure/ServiceAPI/Accounts.ts
+++ client/src/infrastructure/ServiceAPI/Accounts.ts
@@ -1,3 +1,5 @@
+import { Admin } from "../../pages";
+
 export const login = async (email: string, password: string) => {
   const response = await fetch(
     `${import.meta.env.VITE_API_ROOT}/api/v1/login`,
@@ -14,4 +16,30 @@ export const login = async (email: string, password: string) => {
   );
   const json = await response.json();
   return json;
+};
+export const signUp = async (email: string, password: string) => {
+  console.log("dsfsd")
+  try{
+    const response = await fetch( 
+    `${import.meta.env.VITE_API_ROOT}/api/v1/signUp`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        role: Admin
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  console.log("After fet")
+  const json = await response.json();
+  if(response.status != 201) return "Account exists"
+  // return json;
+  }
+  catch(err) {
+    return err;
+  }
 };
--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,11 +1,34 @@
 import { Page } from "../../components";
+import { signUp } from "../../infrastructure/ServiceAPI";
+import { useState} from "react";
 import "./SignUp.style.scss";
 
 function SignUp() {
+  const [message, setMessage] = useState(null);
+  const attemptSignUp = async (email, password) => {
+    try {
+      const message = await signUp(email, password);
+      console.log(message)
+      setMessage(message);
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
+  };
+
   return (
     <Page>
       <div className="signup-page">
         <h1>Sign Up</h1>
+        <div>
+          <input type="text" placeholder="email" id="email" autoFocus></input><br></br>
+          <input type="text" placeholder="password" id="password"></input>
+        </div>
+        <button onClick={() => attemptSignUp((document.getElementById('email')as HTMLInputElement).value, (document.getElementById('password')as HTMLInputElement).value)}>
+          Sign Up
+        </button>
+          {message && <p>{message}</p>}
       </div>
     </Page>
   );
